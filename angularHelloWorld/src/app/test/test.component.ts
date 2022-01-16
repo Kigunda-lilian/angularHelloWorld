607f79eb01d6c784658f0f2720bfca4b79802cbf
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-16 15:16:59 +0300

    add interpolation to display various values , dynamic properties

--- angularHelloWorld/src/app/test/test.component.ts
+++ angularHelloWorld/src/app/test/test.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<div>
+  template: `
+    <h2> Welcome {{name}}</h2>
+    <div>
     inline template
   </div>`,
   styles: [`
@@ -12,7 +14,7 @@ div{
   `]
 })
 export class TestComponent implements OnInit {
-
+public name ="Lilian"
   constructor() { }
 
   ngOnInit(): void {
